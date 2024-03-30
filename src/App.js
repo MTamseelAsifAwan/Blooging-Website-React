ecbd9f2b1e069cd6ffa632a2920a77390c8f0672
MTamseelAsifAwan <muhammadtamseelasif@gmail.com>
2024-03-30 20:45:38 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from "./Navbar";
+import UncontrolledExample from './Hero';
+import AutoLayoutSizingExample from './Section';
+import Footer from './Footer'; // Importing Footer component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  style={{ backgroundColor: '#F0F0F0' }}>
+      
+      <Navbar />
+      <h1 className="my-4 container text-center">labcolabs Blogging Website</h1>
+      <UncontrolledExample />
+      <h3 className="my-4 container text-center">Featured Articles</h3>
+      <AutoLayoutSizingExample />
     </div>
   );
 }
